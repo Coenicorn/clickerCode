3b1a46465adc7df42b27e0a35162a97c92c9317d
Coenicorn <71347039+Coenicorn@users.noreply.github.com>
2021-12-22 23:36:18 +0100

    Update cookieCode.js

--- cookieCode.js
+++ cookieCode.js
@@ -1,3 +1,4 @@
+// adds a shortcut for selling items, as I don't think there is one, there might be tho
 document.addEventListener("keydown", (e)=>{
   if (e.key=="z"&&Game.buyMode>0) {Game.storeBulkButton(1);}
 });
@@ -5,4 +6,30 @@ document.addEventListener("keyup",(e)=>{
   if (e.key=="z") {Game.storeBulkButton(0)}
 });
 
+// display the amount wrinklers have sucked
+(function () {
+    if (!Game.wrinklers.length) return;
+    var b = document.createElement("h1");
+    b.innerHTML = "Coenicorn test";
+    b.style.color = "yellow";
+    b.style.position = "relative";
+    b.style.top = "250px";
+    b.style.fontSize = "20px";
+    b.style.zIndex = 9999999999999;
+    document.getElementById("sectionLeft").appendChild(b);
+    function update() {
+        var t = "Wrinklers have sucked ";
+        var tt = 0;
+        for (var i = 0; i < Game.wrinklers.length; i++) {
+            var c = Game.wrinklers[i];
+            tt += c.sucked;
+        };
+        t += Beautify(tt) + " cookies";
+        b.innerHTML = t;
+        requestAnimationFrame(update);
+    }; 
+    update();
+})();
+
+// scream
 alert("Code added succesfully!");
